c1457db1dfd8736ea1bee997152ee61d0851f1bb
secondcoolestguy <secondcoolestguy@gmail.com>
2026-07-31 08:19:20 -0400

    Update bolma.rb

--- Formula/bolma.rb
+++ Formula/bolma.rb
@@ -2,7 +2,7 @@ class Bolma < Formula
   desc "CLI layout engine for .bolma files written in Rust"
   homepage "https://github.com/secondcoolestguy/bolma"
   url "https://github.com/secondcoolestguy/bolma/archive/refs/tags/v1.0.0pre.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "638231aec96437e2717d23b5793472384c636eff6288644e5b0785ef8cc41d74"
   license "GPL-3.0-only"
 
   depends_on "rust" => :build
